ae82fd86e172fb7ea9ca0b7ff075158808e753b2
Laura Ortiz <lauraortizluque@gmail.com>
2020-06-04 00:50:46 +0200

    arreglar vista

--- resources/views/layouts/private/components/table.blade.php
+++ resources/views/layouts/private/components/table.blade.php
@@ -24,7 +24,7 @@
             @endforeach
             <td>
             <div class ="justify-content-end">
-                <div class= "ml-2 mr-2 row flex-nowrap">
+                <div class= "ml-2 mr-2 row">
                     <a href="{{ route($class.'.show', $item->id)}}" class='btn btn-primary'><img width='14px' src="/images/info.png"></a>
                     <a href="{{ route($class.'.edit', $item->id)}}" class='btn btn-warning'><img width='14px' src="/images/edit.png"></a>
                     <form action="{{ route($class.'.destroy', $item->id)}}" method='post'>
