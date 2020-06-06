5f25cfd5270e1574687ee26864d30ed2fe0f94c1
Laura Ortiz <lauraortizluque@gmail.com>
2020-06-06 21:55:21 +0200

    lo mismo para leaves

--- app/Http/Controllers/LeaveController.php
+++ app/Http/Controllers/LeaveController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Leave;
+use Auth,DB;
 
 class LeaveController extends CrudController
 {
@@ -19,6 +20,37 @@ class LeaveController extends CrudController
         $this->middleware('auth');
         $this->middleware('superadmin', ['only' => 'update']);
         $this->middleware('superadmin', ['only' => 'destroy']);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $currentClass = $this->getCurrentClass();
+        $object = new $currentClass;
+        $aObject = [];
+        $params=$object->getFillable();
+        $readable=[];
+
+        foreach($params as $param){
+            if(substr($param, strlen($param)-3, strlen($param))=='_id'){
+                $str = substr($param,0,strlen($param)-3);
+                $parentClass='\\App\\'.$str;
+                $all=DB::select('select * from '.$parentClass::getAlias());
+                $aObject += [$str=>$all];
+            }
+        }
+
+        if(Auth::user()->role->id =='3'){
+            $readable=['user_id'=>'false'];
+        }
+        
+        return view ('private.'.$currentClass::getAlias().'.create')
+        ->with(['object' => $object,'action' => __FUNCTION__,'parents'=> $aObject])
+        ->with('class',$currentClass::getAlias())->with('readable',$readable);
     }
 
 }
